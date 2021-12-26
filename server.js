c9284866c0f867c91806ce5b1ef3195d585ef02d
Mubariz Hajimuradov <mubarizhajimuradov@gmail.com>
2021-12-26 18:25:52 +0100

    n

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 
+
+const fs = require('fs')
+const https = require('https')
 const WebSocket = require('ws')
+
+
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for the client html page
+const handleRequest = function (request, response) {
+    // Render the single client html file for any request the HTTP server receives
+    console.log('request received: ' + request.url)
+
+    if (request.url === '/client.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript' })
+        response.end(fs.readFileSync('client.js'))
+    } else if (request.url === '/style.css') {
+        response.writeHead(200, { 'Content-Type': 'text/css' });
+        response.end(fs.readFileSync('style.css'));
+    } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(fs.readFileSync('index.html'));
+    }
+};
+
+const httpsServer = https.createServer(serverConfig, handleRequest);
+httpsServer.listen(HTTPS_PORT);
+
+// ----------------------------------------------------------------------------------------
+
+
+
+
 
 const server = new WebSocket.Server({ port: 8080 })
 
