533ca315a35643eee01b7188d937546c41aa4d2d
Mubariz Hajimuradov <mubarizhajimuradov@gmail.com>
2021-12-27 18:20:52 +0100

    https support

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+const https = require('https')
 
 const WebSocket = require('ws')
 
 
 const PORT = process.env.PORT || 3000
 const app = express()
+
+var key = fs.readFileSync(path.join(__dirname + '/certs/key.pem'))
+var cert = fs.readFileSync(path.join(__dirname + '/certs/cert.pem'))
+var options = {
+    key: key,
+    cert: cert
+}
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/index.html'))
@@ -21,12 +31,13 @@ app.get('/style.css', (req, res) => {
 })
 
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+var server = https.createServer(options, app)
+server.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
 
 
-const ws_server = new WebSocket.Server({ port: 8081 })
+const ws_server = new WebSocket.Server({ server: server })
 
 const broadcast = (ws_server, data) => {
     ws_server.clients.forEach((client) => {
